5a98298f2fe5b6060973d2dfd24389f8ae2b946d
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-09-05 18:43:09 +0530

    Update channel-details.js

--- channel-details.js
+++ channel-details.js
@@ -1,6 +1,6 @@
 // sample :   'https://youtube.googleapis.com/youtube/v3/channels?part=brandingSettings&part=statistics&id=UCVl4xmHsrZSkmxYR1zh52eA&key=[YOUR_API_KEY]'
 
-const API_key = `AIzaSyA9LWV77w1tTi_-9RzbYxHDxwPY6KGlbmk`;
+const API_key = `AIzaSyDqAVz32vjipV9DLWfZsQr2S5nF1IW8tB4`;
 
 window.addEventListener("load", () => {
   const cookies = document.cookie.split(";").map((x) => x.trim());
